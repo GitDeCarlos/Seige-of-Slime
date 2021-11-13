22fe90adb39da19adaf3a25e306c0e78101e8316
Carlos Cruz <Carlosincorporated@gmail.com>
2021-11-13 15:19:36 -0600

    Added path following functionality
    Will refine later

--- Seige of Slime/Assets/Scripts/attackerAi.cs
+++ Seige of Slime/Assets/Scripts/attackerAi.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.U2D.Path;
@@ -7,21 +8,57 @@ public class attackerAi : MonoBehaviour
 {
     // Holds info for where to move to next
     public GameObject nextNode;
+    public bool followingNode = true;
     
     // Attacker's stats
     public int health = 100;
-    public double speedMod = 1.0;
-    public float velocity = 1;
+    public float speedMod = 1.0f;
+    public float velocity = 0.1f;
     
-    void FixedUpdate()
+    void Update()
     {
-        Vector3 newPosition; Debug.Log(nextNode.GetComponent<pathNode>().align);
+        if (!followingNode)
+        {
+            return;
+        }
+
+        float direction;
+
+        Vector3 newPosition;
         if (nextNode.GetComponent<pathNode>().align)
-            newPosition = new Vector3(nextNode.transform.position.x, transform.position.y, 0);
+        {
+            // Direction
+            direction = (nextNode.transform.position.y > transform.position.y)? 1.0f : -1.0f;
+            
+            newPosition = new Vector3(nextNode.transform.position.x, transform.position.y + (velocity * speedMod * direction * Time.deltaTime), 0);
+        }
         else
-            newPosition = new Vector3(transform.position.x , nextNode.transform.position.y, 0);
+        {
+            direction = (nextNode.transform.position.x > transform.position.x) ? 1.0f : -1.0f;
+            newPosition = new Vector3(transform.position.x  + (velocity * speedMod * direction * Time.deltaTime), nextNode.transform.position.y, 0);
+        }
         
         this.transform.position = newPosition;
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.name);
+        if (String.Compare(other.name, "Target Node") == 0)
+        {
+            Debug.Log("Collision with node");
+            if (nextNode.GetComponent<pathNode>().tailNode)
+            {
+                followingNode = false;
+                return;
+            }
+            UpdateNode();
+        }
+    }
+
+    void UpdateNode()
+    {
+        this.nextNode = nextNode.gameObject.GetComponent<pathNode>().nextNode;
     }
 }
